0911c8a13ddfcbb891f684eb81c52590433a610c
marcocunhasilva <93876108+marcocunhasilva@users.noreply.github.com>
2022-06-16 23:52:49 -0300

    Data de Nascimento
    Escreva um programa que informa a data do seu nascimento.

---
+++ desafio002.py
@@ -0,0 +1,4 @@
+dia = input('Informe o dia do seu nascimento: ')
+mes = input('Informe o mês (por extenso) do seu nascimento: ')
+ano = input('Informe o ano do seu nascimento: ')
+print('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano, '. Correto?')
---
+++ desafio003.py
@@ -0,0 +1,4 @@
+a = input('1º número: ')
+b = input('2º número: ')
+c = a + b
+print(a, '+', b, '=', c)
